9c6fe0b7ef863c26fe87888401097682662d3731
Amber Mirza <ambermirza89@gmail.com>
2017-02-20 17:05:08 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,20 +1,42 @@
 public class Elevator {
   public int currentLevel; 
   public int destinationLevel;
+  public int status; //-1 going down, 0 not doing anything, 1 going up 
 
   public Elevator() {
     currentLevel = 0; //should be initialized if we figure out what the default level is
   }
   
   public void selectLevel(int destination) {
-    //move elevator to the requested level
-    
-    //set the currentLevel
-    currentLevel = destination;
+    if (destination == currentLevel) {
+      //do nothing
+    } else {
+      
+      //move elevator to the requested level
+      
+      //going down
+      if ( (currentLevel > destination) && (status == 0) ) {
+      
+      
+      //going up
+      } else if ((currentLevel < destination) && (status == 0) {
+        
+        
+      }
+                 
+      //TODO: check status not == 0
+      
+      //set the currentLevel
+      currentLevel = destination;
+    }
   }
   
   public int getLevel() {
     return currentLevel;
   }
 
+  public int getStatus() {
+    return status;
+  }
+  
 }
