c9539e2ae17602f613d7cc0f0b210caf1603d31a
Amber Mirza <ambermirza89@gmail.com>
2017-02-22 16:47:31 -0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -11,9 +11,76 @@ public class Building  {
 		userFloor = floor that the user presses up/down button
 	*/
 	public void request(int direction, int userFloor) {
-		if(one.getStatus() < 0 && direction > 0) {
-			//use elevator 2
-		} else if (two.getStatus()
+		/************user requests to go up************/
+		if (direction > 0) {
+			/////////////figure out which elevator to use//////
+			
+			/* both elevators are not in use */
+			if (one.getStatus() == 0 && two.getStaus() == 0) {
+				/* randomly pick e1 or e2, and use that elevator */
+				//TODO randomly select, and change next line of code
+				call(one, userFloor);
+				
+			/* elevator 1 is going up and user's floor is before destination floor*/	
+			} else if (one.getStatus() > 0 && one.getDestination() > userFloor) {
+				/* use elevator 1, call it to the user's floor */
+				call(one, userFloor);
+				
+			/* elevator 2 is going up and user's floor is before destination floor*/	
+			} else if (two.getStatus() > 0 && two.getDestination() > userFloor) {
+				/* use elevator 2, call it to the user's floor*/
+				call(two, userFloor);
+				
+			/* elevator 1 not in use */	
+			} else if (one.getStatus() == 0) {
+				/* use elevator 1 */
+				call(one, userFloor);
+				
+			/* elevator 2 not in use */	
+			} else if (two.getStatus() == 0) {
+				/* use elevator 2 */
+				call(two, userFloor);
+				
+			} else {
+				/* just use elevator 2 because idk what else to do */
+				call(two, userFloor);
+			}
+				
+		/************user requests to go down************/
+		} else {
+			/////////////figure out which elevator to use//////
+			
+			/* both elevators are not in use */
+			if (one.getStatus() == 0 && two.getStaus() == 0) {
+				/* randomly pick e1 or e2, and use that elevator */
+			
+			/* elevator 1 is going down and user's floor is before destination floor*/	
+			} else if (one.getStatus() < 0 && one.getDestination() < userFloor) {
+				/* use elevator 1 */
+				call(one, userFloor);
+				
+			/* elevator 2 is going down and user's floor is before destination floor*/	
+			} else if (two.getStatus() < 0 && two.getDestination() < userFloor) {
+				/* use elevator 2 */
+				call(two, userFloor);
+				
+			/* elevator 1 not in use */	
+			} else if (one.getStatus() == 0) {
+				/* use elevator 1 */
+				call(one, userFloor);
+
+			/* elevator 2 not in use */	
+			} else if (two.getStatus() == 0) {
+				/* use elevator 2 */
+				call(two, userFloor);
+				
+			} else {
+				/* just use elevator 2 because idk what else to do */
+				call(two, userFloor);
+
+			}		
+		}
+
 	}
 		
 }
