c8e37fd4c2182c478860fc8cb213862ed9b107cf
Amber Mirza <ambermirza89@gmail.com>
2017-02-22 15:49:37 -0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -6,4 +6,14 @@ public class Building  {
 		two = new Elevator();
 	}
 
+	/* 
+		direction = 1 (going up) or -1 (going down)
+		userFloor = floor that the user presses up/down button
+	*/
+	public void request(int direction, int userFloor) {
+		if(one.getStatus() < 0 && direction > 0) {
+			//use elevator 2
+		} else if (two.getStatus()
+	}
+		
 }
